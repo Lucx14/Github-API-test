39b35ba17a901e50fb973d74837a7eae1acd7350
Lucien Najev <luciennajev@hotmail.com>
2018-12-05 20:58:44 +0000

    feature: fetch request to api added to routes, answer display

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const session = require('express-session');
+const fetch = require('node-fetch');
+const developer = require('./models/developer');
 
 // ___APP INITIALIZE___
 const app = express();
@@ -10,10 +13,44 @@ const port = 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// _____SET STATIC FOLDER_____
+app.use(express.static(path.join(__dirname, 'public')));
+
+// _____MIDDLEWARE_____
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'thesecret',
+  saveUninitialized: false,
+  resave: false,
+}));
+
+
 // ___ROUTES___
 app.get('/', (req, res) => res.render('index'));
 
 app.get('/query', (req, res) => res.render('query'));
+
+app.post('/query', (req, res) => {
+  req.session.username = req.body.username;
+  res.redirect('/answer');
+});
+
+app.get('/answer', (req, res) => {
+  // Need to think about what happens when a user inputs nothing in the box
+  // And also how we deal with invalid usernames
+  // So at this point before i even get to my model obeject i want to have validated the username is correct
+  fetch(`https://api.github.com/users/${req.session.username}/repos`)
+    .then(res => res.json())
+    .then((json) => {
+      const dev = new developer(json);
+      let lang = dev.favouriteLanguage();
+      res.render('answer', { language: lang });
+    })
+    .catch(err => {
+      return err;
+    });
+});
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
--- test/feature/feature.js
+++ test/feature/feature.js
@@ -23,4 +23,34 @@ module.exports = {
       .assert.visible('input[name=submit]')
       .end();
   },
+  'Github API query - Answer Page': function(browser) {
+    browser
+      .url('http://localhost:3000/')
+      .click('button[name=query]')
+      .setValue('input[name=username]', 'Lucx14')
+      .click('input[name=submit]')
+      .assert.urlContains('answer')
+      .assert.title('Github API: Answer')
+      .assert.containsText('h4', 'The developers favourite language is probably Ruby')
+      .assert.visible('button[name=query')
+      .assert.visible('button[name=home')
+      .end();
+  },
+  'Github API query - Navigation': function(browser) {
+    browser
+      .url('http://localhost:3000/')
+      .click('button[name=query]')
+      .setValue('input[name=username]', 'Lucx14')
+      .click('input[name=submit]')
+      .assert.urlContains('answer')
+      .click('button[name=query')
+      .assert.urlContains('query')
+      .setValue('input[name=username]', 'Lucx14')
+      .click('input[name=submit]')
+      .assert.urlContains('answer')
+      .click('button[name=home')
+      .assert.urlContains('/')
+      .end();
+  },
+
 }
